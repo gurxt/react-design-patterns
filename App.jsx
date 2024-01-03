9826bbb45e9ebce53faea85307ede511a5d96228
gurxt <jung_jungian@outlook.com>
2024-01-03 16:38:37 -0500

    complete modal design pattern

--- App.jsx
+++ App.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
-import { RegularList } from './components/lists/Regular';
-import { authors } from './data/author';
-import { SmallAuthorListItem } from './components/authors/SmallListItems';
-import { LargeAuthorListItem } from './components/authors/LargeListItems';
-import { books } from './data/books';
+import { Modal } from './components/Modal';
 import { LargeBookListItem } from './components/books/LargeListItems';
-import { SmallBookListItem } from './components/books/SmallListItems';
-import { NumberedList } from './components/lists/Numbered';
+import { books } from './data/books';
 
 export const App = () => {
   return (
     <>
-      <RegularList
-        items={authors}
-        sourceName={'author'}
-        ItemComponent={SmallAuthorListItem}
-      />
-      <RegularList
-        items={authors}
-        sourceName={'author'}
-        ItemComponent={LargeAuthorListItem}
-      />
-      <NumberedList
-        items={books}
-        sourceName={'book'}
-        ItemComponent={SmallBookListItem}
-      />
-      <NumberedList
-        items={books}
-        sourceName={'book'}
-        ItemComponent={LargeBookListItem}
-      />
+      <Modal>
+        <LargeBookListItem book={books[0]} />
+      </Modal>
     </>
   );
 };
