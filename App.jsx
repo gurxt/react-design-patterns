270fe450feaf4cb02db7f45f8803f93fdae0171c
gurxt <jung_jungian@outlook.com>
2024-01-05 20:48:13 -0500

    controlled/uncontrolled modal design pattern

--- App.jsx
+++ App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UncontrolledForm } from './components/UncontrolledForm';
 import { ControlledForm } from './components/ControlledForm';
+import { UncontrolledModal } from './components/UncontrolledModal';
+import { ControlledModal } from './components/ControlledModal';
 
 export const App = () => {
+  const [shouldDisplay, setShouldDisplay] = useState(false);
+
   return (
     <>
-      <ControlledForm />
+      <ControlledModal
+        shouldDisplay={shouldDisplay}
+        onClose={() => setShouldDisplay(false)}
+      >
+        <h3>I am the body of the modal.</h3>
+      </ControlledModal>
+      <button onClick={() => setShouldDisplay(!shouldDisplay)}>
+        {shouldDisplay ? 'Hide Modal' : 'Display Modal'}
+      </button>
     </>
   );
 };
